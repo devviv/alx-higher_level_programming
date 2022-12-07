d1348e6637d5a7c10e9e42a11b72ce1bdef662c6
Devviv <aamehole@gmail.com>
2022-12-07 16:11:37 +0000

    My first for python programming

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+
+rd = "Holberton"
+word_first_3 = 3.word
+word_last_2 =-2.word
+middle_word = 
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
