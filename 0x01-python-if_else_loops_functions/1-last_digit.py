179191623cc197a6fc83291f1175d0c9ca0d0bc9
Devviv <aamehole@gmail.com>
2022-12-20 16:15:03 +0000

    0x01

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+import math
+number = random.randint(-10000, 10000)
+if ((abs(number) % 10) == 0):
+    print(f"Last digit of {number} is {abs(number) %10} and is 0")
+elif ((abs(number) % 10 )< 5):
+    print(f"Last digit of {number} is {abs(number) %10} and is less than 6 and not 0")
+elif ((abs(number) % 10) > 5):
+    print(f"Last digit of {number} is {abs(number) %10} and is greater than 5")
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+ cse(str):
+    for #!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        if (c >= 'a' and c <= 'z'):
+            print(f"{chr(c -32)}.c")
+        else:
+            print(f"{c}.c")
