e261725fbbcc11b6ef64cdf1587e95fdcaa61a92
Devviv <aamehole@gmail.com>
2022-12-21 04:17:26 +0000

    0x01 correction

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,17 @@
 import random
 import math
 number = random.randint(-10000, 10000)
-if ((abs(number) % 10) == 0):
-    print(f"Last digit of {number} is {abs(number) %10} and is 0")
-elif ((abs(number) % 10 )< 5):
-    print(f"Last digit of {number} is {abs(number) %10} and is less than 6 and not 0")
-elif ((abs(number) % 10) > 5):
-    print(f"Last digit of {number} is {abs(number) %10} and is greater than 5")
+if (number < 0):
+    if ((abs(number) % 10) == 0):
+        print(f"Last digit of {number} is -{abs(number) %10} and is 0")
+    elif ((abs(number) % 10 )< 5):
+        print(f"Last digit of {number} is -{abs(number) %10} and is less than 6 and not 0")
+    elif ((abs(number) % 10) > 5):
+        print(f"Last digit of {number} is -{abs(number) %10} and is greater than 5")
+else:
+    if ((abs(number) % 10) == 0):
+        print(f"Last digit of {number} is {abs(number) %10} and is 0")
+    elif ((abs(number) % 10 )< 5):
+        print(f"Last digit of {number} is {abs(number) %10} and is less than 6 and not 0")
+    elif ((abs(number) % 10) > 5):
+        print(f"Last digit of {number} is {abs(number) %10} and is greater than 5")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
- in range(10):
-    for j in range(10):
-        if(i < j):
+#!/usr/bin/python3
+for i in range (10):
+    for j in range (10):
+        if (i < j):
             if(i != 8 or j != 9):
-                print(f"{i}{j}, ",end = " ")
+                print("{}{}".format(i,j),end = ", ")
             else:
-                print(f"{i}{j}",end = " ")
+                print("{}{}".format(i,j))
